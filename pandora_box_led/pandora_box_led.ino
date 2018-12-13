ea02e41dc0e3898eddc32e5a2a606bbc570d2c2c
llmora <lluism@GI01N00514.icepor.com>
2018-12-13 23:43:46 +0100

    Formatter code to fit 80 columns

--- pandora_box_led/pandora_box_led.ino
+++ pandora_box_led/pandora_box_led.ino
@@ -1,17 +1,26 @@
 #include <Adafruit_NeoPixel.h> 
 /*
- * Light up a strip of WS2812 LEDs when a switch is activated, slowly increasing their brightness. When the switch is deactivated the light shuts down.
+ * Light up a strip of WS2812 LEDs when a switch is activated, slowly increasing
+ * their brightness. When the switch is deactivated the light shuts down.
  * 
- * This was used as a "Pandora Box" prop for a drama class play: when the pandora box is open it lits up from the inside, when the box is closed the lights are shut down to save battery.
+ * This was used as a "Pandora Box" prop for a drama class play: when the 
+ * pandora box is open it lits up from the inside, when the box is closed the 
+ * lights are shut down to save battery.
  * 
- * The LED strip "data in" (DI) pin should be tied to pin PB4 in the ATtiny85 and the switch should be tied to pin PB0 (when the switch is open the LEDs switch on)
+ * The LED strip "data in" (DI) pin should be tied to pin PB4 in the ATtiny85 
+ * and the switch should be tied to pin PB0 (when the switch is open the LEDs 
+ * switch on)
  * 
- * The code is based on attiny_pixel_touch by Dana Sniezko (https://github.com/danasf/attiny_pixel_switch/blob/master/attiny_pixel_touch.ino), released under GPLv3
+ * The code is based on attiny_pixel_touch by Dana Sniezko 
+ * (https://github.com/danasf/attiny_pixel_switch/), released under GPLv3
  * 
 */
 
-#define NUM_LEDS 3 // Adjust to the number of leds on your strip - too many leds will require quite a bit of current, make sure your power supply can handle it. We used 3 WS2811 LEDs and powered from a 600mAh 1-cell 3.7V battery
-#define DATA_PIN 4 // PB4 pin on the attiny85
+#define NUM_LEDS 3   // Adjust to the number of leds on your strip - too many 
+                     // leds will require quite a bit of current, make sure your 
+                     // power supply can handle it. We used 3 WS2811 LEDs and 
+                     // powered from a 600mAh 1-cell 3.7V battery
+#define DATA_PIN 4   // PB4 pin on the attiny85
 #define SWITCH_PIN 0 // PB0 pin on the attiny85
 
 // Global variables
@@ -22,7 +31,9 @@ uint8_t switchedOff = 0;
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, DATA_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
-    pinMode(SWITCH_PIN, INPUT_PULLUP); // Enable pull-up resistor on switch pin - when the switch is open the pin goes to HIGH. when it is closed it goes to LOW
+    // Enable pull-up resistor on switch pin - when the switch is open the pin
+    // goes to HIGH. when it is closed it goes to LOW
+    pinMode(SWITCH_PIN, INPUT_PULLUP); 
 
     strip.begin();
     switchOff();
@@ -38,12 +49,14 @@ void loop() {
       fadeIn(0, 255, 2, 50);
     }
   } else {
-    // If the switch is not enabled, switch off the LEDs (who can tell if the LEDs are off when you can't see them!!!)
+    // If the switch is not enabled, switch off the LEDs (who can tell if the 
+    // LEDs are off when you can't see them!!!)
     switchOff();
     fadeShown = 0;
   }
 
-  delay(500); // Save battery in exchange of not-immediate response, a good trade-off as we are aiming for long battery life
+  delay(500); // Save battery in exchange of not-immediate response, a good 
+              // trade-off as we are aiming for long battery life
 }
 
 boolean switchEnabled() {
